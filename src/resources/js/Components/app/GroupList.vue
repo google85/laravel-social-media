2516fdee3320fe511fad7e03944bd61d05355d1c
Florin DjBpF <bpfcomp2005@gmail.com>
2024-02-25 20:53:19 +0200

    [dashboard] make `My Groups` a foldable (Disclosure) area on small screens

--- src/resources/js/Components/app/GroupList.vue
+++ src/resources/js/Components/app/GroupList.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TextInput from '@/Components/TextInput.vue';
 import GroupItem from '@/Components/app/GroupItem.vue';
+import ChevronRightIcon from '@/Components/app/icons/ChevronRightIcon.vue';
 import {ref} from "vue";
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
 
@@ -9,11 +10,14 @@ const searchKeyword = ref('')
 </script>
 
 <template>
-    <div class="px-3 bg-white rounded border py-3 overflow-hidden flex flex-col">
-        <div class="flex-1 block lg:hidden">
+    <div class="px-3 bg-white rounded border h-full py-3 overflow-hidden">
+        <div class="block lg:hidden">
             <Disclosure v-slot="{ open }">
-                <DisclosureButton>
-                    <h2 class="text-xl font-bold">My Groups</h2>
+                <DisclosureButton class="w-full">
+                    <div class="flex justify-between items-center">
+                        <h2 class="text-xl font-bold">My Groups</h2>
+                        <ChevronRightIcon class="transition-all" :class="open ? 'rotate-90 transform' : ''" />
+                    </div>
                 </DisclosureButton>
                 <DisclosurePanel>
                     <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4" />
@@ -59,11 +63,10 @@ const searchKeyword = ref('')
                 </DisclosurePanel>
             </Disclosure>
             </div>
-
-        <div class="hidden lg:block">
+        <div class="h-full overflow-hidden flex-col hidden lg:flex">
             <h2 class="text-xl font-bold mb-4">My Groups</h2>
             <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full" />
-            <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
+            <div class="mt-3 flex-1 overflow-auto">
                 <div v-if="false" class="text-gray-400 text-center p-3">
                     You are not joined to any groups
                 </div>
